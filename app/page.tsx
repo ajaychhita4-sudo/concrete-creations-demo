2a7077f338c1cd3aeecf88cd2b7f338b078fb103
ajaychhita4-sudo <ajaychhita4@gmail.com>
2026-07-02 09:41:37 +1200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
 export default function Home() {
-  return (
-    <div style={{ padding: "40px", textAlign: "center" }}>
-      <h1>Concrete Creations NZ</h1>
-      <p>We build strong, quality concrete work across New Zealand.</p>
-    </div>
-  );
+  return <h1>DEPLOY TEST 123</h1>;
 }
